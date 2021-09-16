a3d7c77ebeebdf305982db5cefb919fbee722e7c
xxx <moldovan.alexandra28@gmail.com>
2021-09-16 16:36:42 +0300

    updating meetups

---
+++ src/coponents/meetups/MeetupList.js
@@ -0,0 +1,20 @@
+import classes from './MeetupList.module.css';
+import MeetupItem from './MeetupItem';
+
+const MeetupList = (props) => {
+    return (
+        <ul className={classes.list}>
+            {props.meetups.map(meetup =>
+                <MeetupItem
+                    key={meetup.id}
+                    id={meetup.id}
+                    image={meetup.image}
+                    title={meetup.title}
+                    adress={meetup.adress}
+                    description={meetup.description}
+                    />)}
+            </ul>
+    )
+}
+
+export default MeetupList;
