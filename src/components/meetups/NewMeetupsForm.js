b4c47777a27e353fabe5cbf355bb7d7ae53b6db8
xxx <moldovan.alexandra28@gmail.com>
2021-09-16 20:20:49 +0300

    add NewMeetupForm

--- src/components/meetups/NewMeetupsForm.js
+++ src/components/meetups/NewMeetupsForm.js
@@ -1,13 +1,38 @@
-import classes from './NewMeetupsForm';
 import Card from '../UI/Card';
+import classes from './NewMeetupForm.module.css';
 
-const NewMeetupsForm = () => {
-    return (
-        <Card>
-        <form className={classes.form}>
-        </form>
-</Card>
-    )
+function NewMeetupForm() {
+  function submitHandler(event) {
+    event.preventDefault();
+
+
+  }
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='title'>Meetup Title</label>
+          <input type='text' required id='title' />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='image'>Meetup Image</label>
+          <input type='url' required id='image' />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='address'>Address</label>
+          <input type='text' required id='address' />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='description'>Description</label>
+          <textarea id='description' required rows='5'></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 
-export default NewMeetupsForm;
+export default NewMeetupForm;
